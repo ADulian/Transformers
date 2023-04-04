53e407c57976c723344d90fe96574143da4fe374
ADulian <duli95@gmail.com>
2023-04-04 11:15:35 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Transformers from scratch
+https://www.youtube.com/watch?v=U0s0f995w14&ab_channel=AladdinPersson
+"""
+
+def main():
+    print("Transformers from scratch")
+
+
+if __name__ == '__main__':
+
+    main()
